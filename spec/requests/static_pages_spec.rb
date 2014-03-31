a38aed98da729ca561b982800fd07502fbb97080
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-03-31 15:47:09 +0200

    Added static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+    it "should have the content 'Microblog'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Microblog')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Home")
+    end
+  end
+
+  describe "Help page" do
+  	it "should have the content 'Help'" do
+		  visit '/static_pages/help'
+      expect(page).to have_content('Help')
+  	end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Help")
+    end
+  end
+
+  describe "About page" do
+    it "should have the content 'About us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About us')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("About us")
+    end
+  end
+
+end
