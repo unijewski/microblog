bf8354698a311c0b1ff3dc88ead68877fbc020f5
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-04-01 21:32:56 +0200

    Added contact page and more tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "Microblog" }
+
   describe "Home page" do
     it "should have the content 'Microblog'" do
       visit '/static_pages/home'
@@ -10,7 +12,7 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Home")
+      expect(page).to have_title("Home » #{base_title}")
     end
   end
 
@@ -22,7 +24,7 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("Help")
+      expect(page).to have_title("Help » #{base_title}")
     end
   end
 
@@ -34,7 +36,19 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title("About us")
+      expect(page).to have_title("About us » #{base_title}")
+    end
+  end
+
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Contact » #{base_title}")
     end
   end
 
