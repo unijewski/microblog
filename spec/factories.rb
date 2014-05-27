5c3b05b62675fee5f4fa22a9ae8bd2f41873db04
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-05-27 13:19:44 +0200

    Working sign up

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Grzegorz Unijewski"
+    email    "grzegorz@unijewski.pl"
+    password "grzesiek"
+    password_confirmation "grzesiek"
+  end
+end
